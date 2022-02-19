fe35c9f1f7b2e23da2b8df17c3dc099a2734be76
muradjan-yunusov <100048490+muradjan-yunusov@users.noreply.github.com>
2022-02-20 04:01:08 +0600

    Add files via upload

---
+++ змейка/chady.js
@@ -0,0 +1,15 @@
+ 
+  <script>
+   function digitalClock() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+       //* добавление ведущих нулей */
+      if (hours < 10) hours = "0" + hours;
+      if (minutes < 10) minutes = "0" + minutes;
+      if (seconds < 10) seconds = "0" + seconds;
+        document.getElementById("id_clock").innerHTML = hours + ":" + minutes + ":" + seconds;
+        setTimeout("digitalClock()", 1000);
+   }
+  </script>
